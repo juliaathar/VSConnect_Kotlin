912328e2b7b229b1bd77dbf58d35716fd9a58f80
juliaathar <juathar@gmail.com>
2024-01-15 21:53:11 -0300

    :construction: Deletado arquivos

--- app/src/main/java/com/example/vsconnect_kotlin/ui/EditarImagemFragment.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/ui/EditarImagemFragment.kt
@@ -1,17 +1,15 @@
-package com.example.vsconnect_kotlin.ui.gallery
+package com.example.vsconnect_kotlin.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.vsconnect_kotlin.databinding.FragmentGalleryBinding
+import com.example.vsconnect_kotlin.databinding.FragmentHomeBinding
 
-class GalleryFragment : Fragment() {
+class EditarImagemFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +21,7 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root
--- app/src/main/java/com/example/vsconnect_kotlin/ui/ListaServicosFragment.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/ui/ListaServicosFragment.kt
@@ -1,17 +1,15 @@
-package com.example.vsconnect_kotlin.ui.home
+package com.example.vsconnect_kotlin.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.vsconnect_kotlin.databinding.FragmentHomeBinding
+import com.example.vsconnect_kotlin.databinding.FragmentGalleryBinding
 
-class HomeFragment : Fragment() {
+class ListaServicosFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +21,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root

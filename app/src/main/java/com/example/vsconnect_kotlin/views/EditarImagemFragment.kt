3991c8091e80d762e47f2495bfe237785a4632d8
juliaathar <juathar@gmail.com>
2024-01-16 21:53:13 -0300

    fazendo config

--- app/src/main/java/com/example/vsconnect_kotlin/views/EditarImagemFragment.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/views/EditarImagemFragment.kt
@@ -1,15 +1,15 @@
-package com.example.vsconnect_kotlin.ui
+package com.example.vsconnect_kotlin.views
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.vsconnect_kotlin.databinding.FragmentHomeBinding
+import com.example.vsconnect_kotlin.databinding.FragmentEditarImagemBinding
 
 class EditarImagemFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentEditarImagemBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,7 +21,7 @@ class EditarImagemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentEditarImagemBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root

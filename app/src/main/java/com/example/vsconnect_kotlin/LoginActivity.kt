53dd1890ed23ca13d9867e694f52bad2441378a3
juliaathar <juathar@gmail.com>
2024-01-18 21:51:49 -0300

    inicio da config de login

--- app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
@@ -2,8 +2,13 @@ package com.example.vsconnect_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vsconnect_kotlin.databinding.ActivityLoginBinding
+
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)

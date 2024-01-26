c0439344572c7602d8f7e7c0b313069df55af2e1
juliaathar <juathar@gmail.com>
2024-01-25 21:50:12 -0300

    fazendo recyclerview

--- app/src/main/java/com/example/vsconnect_kotlin/views/ListaServicosFragment.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/views/ListaServicosFragment.kt
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment
 import com.example.vsconnect_kotlin.apis.EndpointInterface
 import com.example.vsconnect_kotlin.apis.RetrofitConfig
 import com.example.vsconnect_kotlin.databinding.FragmentListaServicosBinding
+import com.example.vsconnect_kotlin.models.Servico
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class ListaServicosFragment : Fragment() {
 
@@ -29,6 +33,17 @@ class ListaServicosFragment : Fragment() {
 
         _binding = FragmentListaServicosBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        endpoints.listarServicos().enqueue(object : retrofit2.Callback<List<Servico>>{
+            override fun onResponse(call: Call<List<Servico>>, response: Response<List<Servico>>) {
+                println(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Servico>>, t: Throwable) {
+            println("Falha na requisição: ${t.message}")
+            }
+
+        })
 
         return root
     }
